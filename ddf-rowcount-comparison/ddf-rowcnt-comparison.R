1f26eb9183d969434382edda245d1a8913ed0be9
walkermj <walkermj.1984@gmail.com>
2018-12-19 10:46:29 +0000

    finalised tweaks and table formatting

--- ddf-rowcount-comparison/ddf-rowcnt-comparison.R
+++ ddf-rowcount-comparison/ddf-rowcnt-comparison.R
@@ -1,6 +1,7 @@
 #-- Name: Script to check integrity of important labkey tables
 #-- Purpose: to check important tables in ddf by counting rows and comparing to previous days count
 #-- Author: mwalker 20181212
+
 #--Clear R environment and set environment options
 rm(list=ls(all=T))
 options(scipen = 200)
@@ -16,28 +17,20 @@ library(wrangleR)
 library(lubridate)
 library(dplyr)
 library(ascii)
-library(gridExtra)
 
 #-- set up / establish slack channel notification
-slackr_setup(channel="datawranglers", 
-          incoming_webhook_url="https://hooks.slack.com/services/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",
-            api_token = 'xoxp-3407199151-239224420817-256880855665-736d7287e5d7fb343361045f87ca54c5')
-
-
-#-- for during testing, to reset prev dataframe to empty 
-#NOTE: REMOVE ONCE AUTOMATING
-# curr = data.frame('participant_id' = character(), 'handling_gmc' = character())
-# saveRDS(curr, "prev/20181100_consent-no.rds")
+slackr_setup(channel="slackr-test", 
+	     username="WranglerBot"
+             incoming_webhook_url="https://hooks.slack.com/services/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",
+             api_token = 'xoxp-3407199151-239224420817-256880855665-736d7287e5d7fb343361045f87ca54c5')
 
 #Connect to db
 drv <- dbDriver("PostgreSQL")
 ddf.con <- dbConnect(drv, dbname = "gel_mi",
-		     	# dbname = "central_data_repo",
-			# host = "10.1.24.37",
-			host = "localhost",
-			# port = 5432,
-			# port = 5439,
-			port = 5440,
+			host = "10.1.24.37",
+			# host = "localhost",
+			port = 5432,
+			# port = 5440,
 			user = "mwalker",
 			password = "password")
 
@@ -56,32 +49,29 @@ rd.ped.member <- dbGetQuery(ddf.con, "SELECT 'cdm.rare_diseases_pedigree_member'
 # disconnect from db 
 dbdisconnectall()
 
-# merge results into single table
+# rowbind results into single dataframe
 cur <- rbind(gel.case, participant, rare.diseases.participant, cancer.participant, consent.form, clinic.sample, lab.sample, plated.sample, rd.fam.medrev, rd.ped.member)
 
 # read in yesterdays row counts
 old  <- readRDS('prev/prev_rowcnt.rds')
 
 # merge and arrive at metrics
-comp <-  left_join(cur, old, by='tbl_name') %>%
-	mutate(diff = cur_tot-old_tot)
+comp <-  left_join(cur, old, by='tbl_name') %>% mutate(diff = cur_tot-old_tot)
+write.csv(comp, file='rowcnts_toPub.csv', row.names=F)
 
 #export todays count for tomorrow
 oldCnts <- dropnrename(cur, c('tbl_name', 'cur_tot'), c('tbl_name', 'old_tot')) # 
-saveRDS(oldCnts, "prev/prev_rowcnt.rds")   # 
+saveRDS(oldCnts, "prev/prev_rowcnt.rds")   
 
-#grid arrange as .png
-# png("counts.png", height=50*nrow(comp), width=200*ncol(comp))
-# p <- tableGrob(format(comp, core.just='left', rows=NULL))
-# grid.arrange(p)
-# dev.off()
+#rename columns for display
+curDate <- format(Sys.time(), '%m/%d')
+yesterDate <- format(Sys.time()-days(1), '%m/%d')
+colnames(comp)  <- c('TABLE', paste(curDate, 'TOT'), paste(yesterDate, 'TOT'), 'DIFF')
 
-# ascii table
-output <- ascii(comp, include.rownames = F, format=c('s', 'd', 'd'), header = F)  
+# turn into ascii table
+mis.data.checks <- ascii(comp, include.rownames = F, format=c('s', 'd', 'd'), header = F)  
 
-slackr_msg(paste('@here', 
-		'\n Latest DDF data checks (todays row counts against yesterdays):\n'))
-		slackr(output)
-		# httr::with_verbose(slackr_upload("counts.png"))
-		slackr_msg('\n\n:robot_face: _This is an automated message_ :robot_face:')
-		# saveRDS(consent.nos[["current"]], paste("prev/", curDate, "_consent-no.rds", sep=""))
+slackr_msg(as_user=F,username='WranglerBot', paste('@here', 
+		'\n Latest MIS data checks (todays row counts against yesterdays):\n'))
+		slackr(mis.data.checks)
+		slackr_msg(as_user=F, username='WranglerBot','\n\n:robot_face: _This is an automated message_ :robot_face:')
